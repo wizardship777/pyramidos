f1806d0c24f3e5e86171afdf8b1cb5838116868e
wizardship777 <buddhathemagus@gmail.com>
2024-06-10 01:46:20 +0530

    Create qchft.cpp

---
+++ qchft.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    /*
+        Quantum Computer Algorithms Dequantified for Financial Markets for MFT/HFT (trade every 1h, 1m, etc) 
+        TODO: By 3,000,000 AD
+    */
+}
