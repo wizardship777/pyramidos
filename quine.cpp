f1a3ea548969322c7dd3671ad82df2cf9f995293
wizardship777 <buddhathemagus@gmail.com>
2024-06-10 02:26:40 +0530

    Update quine.cpp

--- quine.cpp
+++ quine.cpp
@@ -8,5 +8,6 @@ int main() {
         Quine,
         manifest wealth, abundance, prosperity, etc (related to jupyter?) with proofs. 
         With ZK-SNARKS?
+        Citadel HFT, MFT, ? Algorithms -> C++, modified acc. to codestyle, easy to backtest. 2021-2500AD?
     */
 }
