811a2ab166107421bd44040691e9dcc1a2e0c83b
wizardship777 <buddhathemagus@gmail.com>
2024-06-10 04:00:07 +0530

    Create force.cpp

---
+++ force.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    /*
+        APPEND: TODO/FIX by \inf+ AD:
+        //int -> bigint?
+    */
+    string secret_artifact = "novel_blockchain_with_decentralized_algorithmic_stablecoin"
+    string cunei_form_like = "Jesus_Christ_pyramidos_form";
+    string cataclysm = "Archangel_Michael_pyramidos_cataclysm";
+    //UKKONEN(APPEND, secret_artifact) -> tree vertex
+    //tree vertex -> point t. embedding of UKKONEN on //APPEND based on ? on ABOVE
+    //Voronoi Diagram -> force based on above 
+    //auto APPEND above?
+
+}
