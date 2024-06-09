0f89da3595a5dee43ea69b1ede005ef0b92369d5
wizardship777 <buddhathemagus@gmail.com>
2024-06-10 01:55:05 +0530

    Create quine.cpp

---
+++ quine.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    /*
+        TODO by \inf+ AD:
+        Quine,
+        manifest wealth, abundance, prosperity, etc (related to jupyter?) with proofs. 
+        With ZK-SNARKS?
+    */
+}
