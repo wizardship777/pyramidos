782333d7e9f6a63b4b84051dbf5d527c3b99fe4e
wizardship777 <buddhathemagus@gmail.com>
2024-06-10 02:19:55 +0530

    Update nanotechnology.cpp

--- nanotechnology.cpp
+++ nanotechnology.cpp
@@ -8,4 +8,18 @@ int main() {
         Nanotechnology chips:
         Manifestation, Magick, Future Science, (Knowledge Graph AGI) -> Understanding? Word Description?
     */
+    /*
+        APPEND: TODO/FIX by \inf+ AD:
+        //int -> bigint?
+    */
+    vector <vector <int> > group_permutations;//APPEND
+    int n = (int) group_permutations.size();
+    int seed = time(0);//APPEND
+    mt19937 rnd(seed);//APPEND
+    auto perm = group_permutations[rnd() % n];
+    for (int i = 0; i < n; i++) {
+        //link i, perm[i] APPEND
+        //check electricity in processor APPEND
+        //build M.S. -> ASI -> nanochip APPEND
+    }
 }
