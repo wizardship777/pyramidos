8a48d478a50b67cde61bee0596a57a03451b64e7
wizardship777 <buddhathemagus@gmail.com>
2024-06-10 04:24:48 +0530

    Create shuffler.cpp

---
+++ shuffler.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    /*
+        APPEND: TODO/FIX by \inf+ AD:
+        //int -> bigint?
+    */
+    vector <int> gate_ids(64);//APPEND
+    mt19937 rnd(time(0));//APPEND
+    vector <int> tc(64);
+    for (int i = 0; i < 64; i++) {
+        int j = rnd() % 64;//ARCHANGEL MICHAEL TAROT APPEND
+        tc[i] = j;
+    }
+    for (int i = 0; i < 64; i++) {
+        //link gate_ids[i], gate_ids[tc[i]]
+    }
+    //I CHING DIVINATIONS FROM JESUS CHRIST
+    //auto APPEND above?
+
+}
