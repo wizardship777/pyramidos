9a73faac9529ae3d272818737a83a0af739424bb
wizardship777 <buddhathemagus@gmail.com>
2024-06-10 04:56:30 +0530

    Create gamefi.cpp

---
+++ gamefi.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    /*
+        APPEND: TODO/FIX by \inf+ AD:
+        //int -> bigint?
+    */
+    mt19937 rnd(time(0));//APPEND
+    //WHO APPEND
+    int n = rnd();//APPEND
+    vector <vector <int> > map(n, vector <int> (n));//APPEND
+    //2d coordinates -> 3d points on a figure APPEND
+    vector <string> actions(n);//APPEND
+    vector <int> dynamics(n * n * n);//APPEND
+    vector <int> finances(n * n);//APPEND
+    vector <string> shards(n * n * n);//APPEND
+    vector <string> cluster_shards(n * n);//APPEND
+    vector <string> qc_dequantification_t_game_design(n * n);//APPEND
+    vector <double> decentralized_internet_local_hft(n * n * n * n);
+    for (int i = 0; i < n * n * n * n; i++)//POST-QUANTUM SUPREMACY 
+    {
+        int shard_id = rnd();//TQF WHO
+        decentralized_internet_local_hft[i] = shard_id;
+    }
+    //run(), set_up()
+    //AGI ASI AGI ASI ORDER? APPEND ABOVE
+    //WHO ABOVE?
+    //APPEND above?
+}
