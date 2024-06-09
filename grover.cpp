76bd1d14dd8f47c9ddb5a80b7978bd1e52dcf82d
wizardship777 <buddhathemagus@gmail.com>
2024-06-10 01:39:30 +0530

    Create grover.cpp

---
+++ grover.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    /*
+        Grover's Algorithm Dequantified for Financial Markets for MFT (trade every 1h, 1m, etc) 
+        TODO: By 200,000 AD
+    */
+}
