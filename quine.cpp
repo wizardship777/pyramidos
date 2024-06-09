977ad9be0a07b07ce633e2dd4ba198fd6afeba33
wizardship777 <buddhathemagus@gmail.com>
2024-06-10 02:56:28 +0530

    Update quine.cpp

--- quine.cpp
+++ quine.cpp
@@ -9,7 +9,7 @@ int main() {
         manifest wealth, abundance, prosperity, etc (related to jupyter?) with proofs. 
         With ZK-SNARKS?
         Citadel, Wintermute, Jump, Dragonfly?, (...), HFT, MFT, ? Algorithms -> C++, modified acc. to codestyle, easy to backtest. 2021-2500AD?
-        DeFi startups, cryptocurrency startups -> ...APPEND to codestyle, easy to backtest. 2021-2500AD?
+        My good IDEAS, ETC from \inf+ AD to PM, DR, CR, ... -> ...APPEND to CHIP, easy to backtest. 2021-2500AD? CHIP APPEND AGI AGI AGI 
 
     */
 }
