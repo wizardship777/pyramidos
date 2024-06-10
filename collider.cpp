7b980b8b925ffbd00f8c75a468b01531c98f1eb2
wizardship777 <buddhathemagus@gmail.com>
2024-06-10 16:36:32 +0530

    Create collider.cpp

---
+++ collider.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    /*
+        APPEND: TODO/FIX by \inf+ AD:
+        //int -> bigint?
+    */
+    mt19937 rnd(time(0));//APPEND
+    vector <vector <double> > qi_flows;
+    vector <vector <double> > colliders;
+    channel(qi_flows, colliders);
+    projective_transformations(colliders, fractals);
+    reignite_pheonix(fractals, brainchips);
+    philosopher_stone(brainchips, body, environment);
+    spooky_action_at_a_distance = evaluate_spook(environment);
+    change_reality_by_mind(spooky_action_at_a_distance, colliders);
+    //APPEND sim to ...
+}
