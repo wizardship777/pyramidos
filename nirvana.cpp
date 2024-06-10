0da354a99450fc478d9eb9245e0d80657260b323
wizardship777 <buddhathemagus@gmail.com>
2024-06-10 17:18:23 +0530

    Create nirvana.cpp

---
+++ nirvana.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    /*
+        APPEND: TODO/FIX by \inf+ AD:
+        //int -> bigint?
+    */
+    mt19937 rnd(time(0));//APPEND
+    vector <vector <double> > brain;
+    vector <vector <double> > body;
+    auto dequantifier_approximator_path_equivalence = [&] (vector <double> reality_slices, vector <double> possible_shifts) {
+        auto delta = through_golgotha_circulate(brain, reality_slices, possible_shifts, body);
+        return delta;
+    };
+    while (!filled(black_holes, wu_convolution)) {
+        auto qc_program = dequantifier_approximator_path_equivalence(reality_slices, possible_shifts);
+        dna_evolve(qc_program);
+    }
+    //APPEND sim to ...
+}
