e10fec57e4bd9585a130c727a69913cce12b39b0
wizardship777 <buddhathemagus@gmail.com>
2024-06-10 06:15:31 +0530

    Create casino.cpp

---
+++ casino.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    /*
+        APPEND: TODO/FIX by \inf+ AD:
+        //int -> bigint?
+    */
+    mt19937 rnd(time(0));//APPEND
+    //WHO APPEND
+    GAMBLING_BANDIT_STRATEGIES_RL_MAGICK_VERSUS_CASINO_AFRICAN_VODOU;
+    //AGI ASI AGI ASI WHICH? HOW MANY? ORDER? APPEND ABOVE
+    //WHO ABOVE?
+    //APPEND above?
+}
