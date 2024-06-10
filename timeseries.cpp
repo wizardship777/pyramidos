b9609180f5dbd6a01a6a810bc2fb4446c5c57f82
wizardship777 <buddhathemagus@gmail.com>
2024-06-10 07:12:29 +0530

    Create timeseries.cpp

---
+++ timeseries.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    /*
+        APPEND: TODO/FIX by \inf+ AD:
+        //int -> bigint?
+    */
+    mt19937 rnd(time(0));//APPEND
+    //WHO APPEND
+    //+2000 years -> NN FIT +n years -> pieces of 2000 years or t years
+    //wait until equilibrium is reached, then can reduce t -> increase n?
+    //(n,t) -> t->0, n->\infty (Large of Law Numbers + Bandit)
+    //APPEND sim. to ...
+    //APPEND SOME GITHUB \inf+ AD
+    //
+}
