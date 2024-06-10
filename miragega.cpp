ee0192a70e73733d07c51aab5591834e8b786efa
wizardship777 <buddhathemagus@gmail.com>
2024-06-10 21:59:00 +0530

    Create miragega.cpp

---
+++ miragega.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    /*
+        APPEND: TODO/FIX by \inf+ AD:
+        //int -> bigint?
+    */
+    mt19937 rnd(time(0));//APPEND
+    //WHO APPEND
+    int n = rnd();//APPEND
+    vector <double> mirage_of_life(n + 3);
+    vector <double> mirage_of_game(n + 7);
+    genetic_algorithms(mirage_of_life, mirage_of_game, DO YOU KNOW THE WAE KNUCKCLES CODE GENERATOR FUNCTIONS);
+    //AGI ASI AGI ASI WHICH? HOW MANY? ORDER? APPEND ABOVE
+    //WHO ABOVE?
+    //APPEND above?
+}
